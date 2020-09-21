dcdd5bd17a798471af0f12da51529274156fc4f9
Mykhailo Zub <mikhail.a.zub@gmail.com>
2020-09-21 19:58:26 +0300

    Updates

---
+++ precache-manifest.67c79b85cbf9c0e9449df68a72ce638e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71656d53b48659508ebe5f08642ca11",
+    "url": "/HW18-ReactJS.Styled/index.html"
+  },
+  {
+    "revision": "3238881348db9b891fcb",
+    "url": "/HW18-ReactJS.Styled/static/css/main.e9e6e350.chunk.css"
+  },
+  {
+    "revision": "4d7c2f5a1ab0220cc539",
+    "url": "/HW18-ReactJS.Styled/static/js/2.ca3b9fae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HW18-ReactJS.Styled/static/js/2.ca3b9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3238881348db9b891fcb",
+    "url": "/HW18-ReactJS.Styled/static/js/main.f4c3027c.chunk.js"
+  },
+  {
+    "revision": "37a95e76cf5770bd5344",
+    "url": "/HW18-ReactJS.Styled/static/js/runtime-main.e183555b.js"
+  },
+  {
+    "revision": "d964e0b3c644193e5591758f01e91f68",
+    "url": "/HW18-ReactJS.Styled/static/media/padlock.d964e0b3.svg"
+  }
+]);
